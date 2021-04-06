861f142b1c4f4b2c4c937e2fdd98e82662a3d9c8
Jun Tian <tianjun.cpp@gmail.com>
2021-04-06 09:19:56 +0800

    fix CI error

--- books/zh/build.jl
+++ books/zh/build.jl
@@ -4,10 +4,10 @@ using JuliaDevelopersToolkit
 function install_fonts()
     fonts_dir = joinpath(homedir(), ".fonts")
     run(`wget -q -O tmp.zip https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCJKsc-hinted.zip`)
-    run(`unzip tmp.zip $fonts_dir`)
+    run(`unzip tmp.zip -d $fonts_dir`)
     run(`rm tmp.zip`)
     run(`wget -q -O tmp.zip https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifCJKsc-hinted.zip`)
-    run(`unzip tmp.zip $fonts_dir`)
+    run(`unzip tmp.zip -d $fonts_dir`)
     run(`rm tmp.zip`)
     run(`fc-cache --verbose $fonts_dir`)
 end
