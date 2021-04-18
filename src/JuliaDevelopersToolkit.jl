5cac397a23fd7b5ac5a18441274ab3c584e63655
Jun Tian <tianjun.cpp@gmail.com>
2021-04-19 01:12:44 +0800

    add last update in landing page

--- src/JuliaDevelopersToolkit.jl
+++ src/JuliaDevelopersToolkit.jl
@@ -1,6 +1,12 @@
 module JuliaDevelopersToolkit
 
+using Dates
+
 greet() = "Hello World!"
+
+last_update() = """
+    Last Update: $(now())
+    """
 
 current_julia_version_zh() = """
     本书所使用的 Julia 版本为 $VERSION ，建议读者阅读本书时使用该版本或者是更新的版本。
